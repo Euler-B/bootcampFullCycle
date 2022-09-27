10d019489b03a2f1ff929dba83e4fdfb0caa545e
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-09-27 18:47:32 -0400

    first day

--- internal/order/entity/order_test.go
+++ internal/order/entity/order_test.go
@@ -11,3 +11,30 @@ func TestGivenAnEmptyId_WhenCreateaNewOrder_ThenShouldReceiveAndError(t *testing
 	order := entity.Order{}
 	assert.Error(t, order.IsValid(), "invalid id")
 }
+
+func TestGivenAnEmptyPrice_WhenCreateaNewOrder_ThenShouldReceiveAndError(t *testing.T){
+	order := entity.Order{ID: "123"}
+	assert.Error(t, order.IsValid(), "invalid price")
+}
+
+func TestGivenAnEmptyTax_WhenCreateaNewOrder_ThenShouldReceiveAndError(t *testing.T){
+	order := entity.Order{ID: "123", Price: 10}
+	assert.Error(t, order.IsValid(), "invalid tax")
+}
+
+func TestGivenAValidParams_WhenCalltNewOrder_ThenShould_ReceiveCreateOrderWithAllParams(t *testing.T) {
+	order, err := entity.NewOrder("123", 10 , 2)
+	assert.NoError(t, err)
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 2.0, order.Tax)
+}
+
+func TestGivenAValidParams_WhenCallCalculateFinalPrice_ThenShouldCalculateFinalPriceAndSetItOnFinalPriceProperty(t *testing.T) {
+	order, err := entity.NewOrder("123", 10 , 2)
+	assert.NoError(t, err)
+	err = order.CalculateFinalPrice()
+	assert.NoError(t, err)
+	assert.Equal(t, 12.0, order.FinalPrice)
+}
+
